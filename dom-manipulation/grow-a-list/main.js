0c7f2c69588494e825a084e8a6c7b086c39a160c
matt_moe <mrobertmoe@gmail.com>
2019-09-17 16:22:10 -0700

    grabbed list, and children, and added to existing list

--- dom-manipulation/grow-a-list/main.js
+++ dom-manipulation/grow-a-list/main.js
@@ -2,7 +2,16 @@ function test() {
   //grab "parent", which is the list we want to append to
   //querySelectorAll returns a nodeList, getElementsByClassName returns an HTML collection
   //querySelector and getElementsById returns the HTML element
-  
+  let list = document.querySelector('ol.list-container');
+  let children = list.children;
+
   //Grab lastChild, find the last ID# create a new List incrementing the ID# and item# class
+  let lastChild = children[children.length - 1];
+  let newChildId = parseInt(lastChild.id) + 1;
+  let newChild = document.createElement('li');
+  newChild.id = newChildId;
+  newChild.innerHTML = 'Fourth Item ' + `<button onclick=''>Remove</button>`;
+  newChild.className = `item item-${newChildId}`;
+  list.appendChild(newChild);
   //Appened the newChild to the "parent", which is the list
 }
