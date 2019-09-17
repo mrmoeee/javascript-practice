debb3c46bda4b38bb359371f6e7876643ab3a2ca
matt_moe <mrobertmoe@gmail.com>
2019-09-17 16:21:42 -0700

    added comments to guide me

---
+++ dom-manipulation/grow-a-list/main.js
@@ -0,0 +1,8 @@
+function test() {
+  //grab "parent", which is the list we want to append to
+  //querySelectorAll returns a nodeList, getElementsByClassName returns an HTML collection
+  //querySelector and getElementsById returns the HTML element
+  
+  //Grab lastChild, find the last ID# create a new List incrementing the ID# and item# class
+  //Appened the newChild to the "parent", which is the list
+}
