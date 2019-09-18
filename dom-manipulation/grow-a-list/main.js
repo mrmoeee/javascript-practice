83e4aee8621f03598a1653d4311975b4028fd335
matt_moe <mrobertmoe@gmail.com>
2019-09-18 11:41:57 -0700

    Added remove feature

--- dom-manipulation/grow-a-list/main.js
+++ dom-manipulation/grow-a-list/main.js
@@ -1,17 +1,37 @@
-function test() {
+function addLi() {
   //grab "parent", which is the list we want to append to
   //querySelectorAll returns a nodeList, getElementsByClassName returns an HTML collection
   //querySelector and getElementsById returns the HTML element
   let list = document.querySelector('ol.list-container');
   let children = list.children;
 
-  //Grab lastChild, find the last ID# create a new List incrementing the ID# and item# class
+  //If list is empty start the index at 1
+  //Else, grab lastChild, find the last ID# create a new List incrementing the ID# and item# class
   let lastChild = children[children.length - 1];
-  let newChildId = parseInt(lastChild.id) + 1;
+  let newChildId = 1;
+  if(children.length) {
+    console.log('Empty Children!');
+  }  else {
+    // newChildId = 29;
+  }
+
+
   let newChild = document.createElement('li');
   newChild.id = newChildId;
-  newChild.innerHTML = 'Fourth Item ' + `<button onclick=''>Remove</button>`;
+  //innerHTML is prone to XSS when recieving user input, here we are not so its fine(?)
+  newChild.innerHTML = 'Some item name' + ' ' + `<button onclick='removeLi(this)'>Remove</button>`;
   newChild.className = `item item-${newChildId}`;
-  list.appendChild(newChild);
   //Appened the newChild to the "parent", which is the list
+  list.appendChild(newChild);
+}
+
+function removeLi(node) {
+  //remove buttons parent is a list because its inside the li html element
+  let nodeLi = node.parentElement;
+  let nodeOl = nodeLi.parentElement;
+  nodeOl.removeChild(nodeLi);
+
+  if(node) {
+    console.log(node.parentElement);
+  }
 }
