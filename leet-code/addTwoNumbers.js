8be773731e560f51b5a63e2c04f4b822033a38a8
matt_moe <mrobertmoe@gmail.com>
2019-09-27 10:48:35 -0700

    added comments and handle cases

--- leet-code/addTwoNumbers.js
+++ leet-code/addTwoNumbers.js
@@ -4,24 +4,34 @@
 // Add the two numbers and return it as a linked list.
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
+//using linked list to perform addition
+// 123 
+//+567
+//----
+// 3+7 = 10, carry the 1 -> 0
+// 6+2 = 8, add the carry, return carry back to 0 -> 9 carry = 0
+// 5+1 = 6
+//answer = 690
+
+//l1 = 3 -> 2 -> 1 -> null
+//l2 = 7 -> 6 -> 5 -> null
+//carry : 1, carry : 0
+//l3 = 0 -> 9 -> 6 -> null
+//linked list is linear data structure
+//need dummy for dummy.next 
+//cases to handle
+//1: input of different length
+//2: input result that generates an extra list node to be added due to carry
+
 class Node {
   constructor(val) {
     this.value = val;
     this.next = null;
   }
 }
-//creating l1 and l2 as linkedlists
-let l1 = new Node(1);
-l1.next = new Node(4);
-// l1.next.next = new Node(3);
 
-let l2 = new Node(1);
-// l2.next = new Node(6);
-// l2.next.next = new Node(4);
-// let l1 = new Node(0);
-// let l2 = new Node(0);
-// console.log(l1);
-//l1 = (2 -> 4 - >3) + (l2) (5 -> 6 -> 4);
-//expected output = 7 -> 0 -> 8
+function addTwoNumbers(l1, l2) {
+  let dummy = new Node(0);
+}
 
 
